5f673e9f141fa3d3ceef30f5251bf2808577eb29
Mughil97 <mmughil97@gmail.com>
2023-11-16 22:43:07 +0530

    Working frontend code. Time to break it :P

--- frontend/js/themeToggle.js
+++ frontend/js/themeToggle.js
@@ -1,16 +1,50 @@
-// themeToggle.js
+document.getElementById('themeToggle').addEventListener('click', function () {
+    let currentTheme = document.body.dataset.theme;
 
-function toggleTheme() {
-    var body = document.body;
-    var button = document.getElementById('theme-toggle');
-
-    // Toggle the 'dark-theme' class on the body
-    body.classList.toggle('dark-theme');
-
-    // Check if the body has the 'dark-theme' class
-    if (body.classList.contains('dark-theme')) {
-        button.textContent = '🌙';
+    // Toggle between light and dark theme
+    if (currentTheme === "dark") {
+        document.body.dataset.theme = "light";
+        document.querySelector('.content').toggleAttribute('hidden');
+        document.getElementById("bulb").src = "assets/light-bulb-on.svg";
     } else {
-        button.textContent = '🌞';
+        document.body.dataset.theme = "dark";
+        document.querySelector('.content').toggleAttribute('hidden');
+        document.getElementById("bulb").src = "assets/light-bulb.svg";
     }
-}
+});
+
+document.getElementById('checkMessage').addEventListener('click', function () {
+    const userInput = document.getElementById('decryptedMessage').value.toUpperCase();
+
+    // Call the API here
+    fetch('https://darth97.dev/api/v1/check', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            secret: userInput
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.redirect) {
+                // Directly open the URL in a new tab
+                window.open(data.redirect, '_blank');
+            } else if (data.status) {
+                confetti({
+                    particleCount: 100,
+                    spread: 70,
+                    origin: { y: 0.6 }
+                });
+                var myModal = new bootstrap.Modal(document.getElementById('winModal'), {});
+                myModal.show();
+            } else {
+                alert(data.hint)
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
+
